8dc91892bcba3943c2b619ca0f93268cf3288e26
Eugene Taranov <eugene@opsfleet.com>
2020-06-24 15:09:36 +0300

    added sonar runner home dir

--- dock-run.sh
+++ dock-run.sh
@@ -2,10 +2,8 @@
 
 tag=$(echo ${PWD} | tr / - | cut -b2- | tr A-Z a-z)
 groups=$(id -G | xargs -n1 echo -n " --group-add ")
-echo "{{{****}}}"
-id -u
-echo "{{{****}}}"
-params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd -v/etc/group:/etc/group -e SONAR_ORGANIZATION -e SONAR_PROJECT_KEY -e SONAR_URL -e SONAR_LOGIN ${tag}"
+mkdir /tmp/sonar-runner/
+params="-v ${PWD}:${PWD} -v /tmp/sonar-runner:/home/runner --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd -v/etc/group:/etc/group -e SONAR_ORGANIZATION -e SONAR_PROJECT_KEY -e SONAR_URL -e SONAR_LOGIN ${tag}"
 
 cd docker
 docker build --tag=${tag} .
