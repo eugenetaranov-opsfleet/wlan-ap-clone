e461344eaddbcf84855a83b95035f66822c275cf
Eugene Taranov <eugene@opsfleet.com>
2020-06-24 15:02:46 +0300

    debug

--- dock-run.sh
+++ dock-run.sh
@@ -2,6 +2,9 @@
 
 tag=$(echo ${PWD} | tr / - | cut -b2- | tr A-Z a-z)
 groups=$(id -G | xargs -n1 echo -n " --group-add ")
+echo "{{{****}}}"
+id -u
+echo "{{{****}}}"
 params="-v ${PWD}:${PWD} --rm -w ${PWD} -u"$(id -u):$(id -g)" $groups -v/etc/passwd:/etc/passwd -v/etc/group:/etc/group -e SONAR_ORGANIZATION -e SONAR_PROJECT_KEY -e SONAR_URL -e SONAR_LOGIN ${tag}"
 
 cd docker
